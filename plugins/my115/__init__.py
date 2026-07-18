d0bf455d5eee23776adc557637ac0159ca379c6d
AOTUMAN133 <aotuman133@github.com>
2026-07-18 23:43:31 +0000

    bump: my115 v1.1.3→v1.1.4

--- plugins/my115/__init__.py
+++ plugins/my115/__init__.py
@@ -19,7 +19,7 @@ from ._tmdb import TmdbApi, emby_has_tmdb_id, get_emby_tmdb_ids
 __plugin__ = {
     "name": "115频道监控",
     "id": "my115",
-    "version": "1.1.3",
+    "version": "1.1.4",
     "author": "凹凸曼",
     "description": "通用监控频道里的 115 分享，读取/识别 TMDB 后查 Emby 媒体库，缺失的转发给 CMS 入库机器人。可选电影/电视剧，默认全部。",
     "scope": "user",
