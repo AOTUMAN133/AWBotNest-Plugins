b1fb0146cf8bba4ce33dc6a5b80e48a828587923
AOTUMAN133 <aotuman133@github.com>
2026-08-01 01:20:44 +0000

    bili_search v1.2.5 / myai v1.5.0 / mydc v1.2.6 / videodl v2.4.5

--- plugins/mydc/__init__.py
+++ plugins/mydc/__init__.py
@@ -11,7 +11,7 @@ TZ = timezone(timedelta(hours=8))
 __plugin__ = {
     "name": "DC助手",
     "id": "mydc",
-    echo "version: 1.2.$(( $(echo "version": "1.2.6" | grep -oP d+) + 1 ))",
+    "version": "1.2.6",
     "icon": "https://raw.githubusercontent.com/AOTUMAN133/AWBotNest-Plugins/main/plugins/icons/mydc_v2.svg",
     "author": "凹凸曼",
     "description": "配合 DockerCopilot 实现容器自动更新、清理、备份。",
